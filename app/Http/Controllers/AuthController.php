d6ffa54436488e32310ac7ad9d01aa164202003b
Iury Veloso <iurysveloso@gmail.com>
2025-08-06 13:35:26 -0300

    feat: Auth and User endpoints

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a user.
+     */
+    public function register(Request $request)
+    {
+        $messages = [ 
+            'name.required' => 'O Nome é obrigatório!',
+            'name.max' => 'O Nome deve ter menos de 255 caracteres!',
+            'email.required' => 'O Email é obrigatório!',
+            'email.max' => 'O Email deve ter menos de 255 caracteres!',
+            'email.email' => 'O Email deve ser válido!',
+            'email.unique' => 'Este Email Já existe!',
+            'file.required' => 'O Avatar de perfil é obrigatório!',
+            'file.file' => 'O Avatar de perfil deve ser um arquivo válido!',
+            'file.mimes' => 'O Avatar de perfil deve está somente em .png ou .jpg!',
+            'file.max' => 'O Avatar de perfil deve ter menos de 10MB!',
+            'password.required' => 'A Senha é obrigatória!',
+            'password.confirmed' => 'A Senha e Confirmação de Senha devem ser iguais!',
+        ];
+
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|email|unique:users',
+            'file' => 'required|file|mimes:png,jpg|max:10240',
+            'password' => 'required|confirmed'
+        ], $messages);
+
+        // http://localhost:8000/storage/uploads/{filename}
+        $request->file('file')->storePublicly('uploads', 'public');
+        $file_name = $request->file('file')->hashName();
+
+        $user = new User;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->avatar = $file_name;
+        $user->password = $request->password;
+
+        $user->save();
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    /**
+     * Login with a user account.
+     */
+    public function login(Request $request)
+    {
+        $messages = [ 
+            'email.required' => 'O Email é obrigatório!',
+            'email.max' => 'O Email deve ter menos de 255 caracteres!',
+            'email.email' => 'O Email deve ser válido!',
+            'email.unique' => 'Este Email Já existe!',
+            'password.required' => 'A Senha é obrigatória!',
+            'password.confirmed' => 'A Senha e Confirmação de Senha devem ser iguais!',
+        ];
+
+        $request->validate([
+            'email' => 'string|required|max:255|email|exists:users',
+            'password' => 'required',
+        ], $messages);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return [
+                'message' => 'As credenciais estão incorretas!'
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    /**
+     * Logout with a user account.
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return [
+            'message' => 'Você saiu do sistema.'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)->name('auth.')->group(function () {
+    Route::post('register', 'register')->name('register');
+    Route::post('login', 'login')->name('login');
+    Route::post('logout', 'logout')->name('logout')->middleware('auth:sanctum');
+});
+
+Route::controller(UserController::class)->name('user.')->middleware('auth:sanctum')->group(function () {
+    Route::get('user', 'show')->name('show');
+    Route::patch('user/update', 'update')->name('update');
+    Route::post('user/update/avatar', 'updateAvatar')->name('update.avatar');
+    Route::patch('user/update/password', 'updatePassword')->name('update.password');
+});
 
 Route::apiResource('notes', NoteController::class)->middleware('auth:sanctum');
 Route::controller(NoteController::class)->name('notes.')->group(function () {
